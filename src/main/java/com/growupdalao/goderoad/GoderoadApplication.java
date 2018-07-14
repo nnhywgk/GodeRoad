e971fbae2fc3c096f280c071c7d063ecdf388620
adme <v9x@qq.com>
2018-07-14 17:42:29 +0800

    first commit

---
+++ src/main/java/com/growupdalao/goderoad/GoderoadApplication.java
@@ -0,0 +1,12 @@
+package com.growupdalao.goderoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoderoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoderoadApplication.class, args);
+	}
+}
---
+++ src/test/java/com/growupdalao/goderoad/GoderoadApplicationTests.java
@@ -0,0 +1,16 @@
+package com.growupdalao.goderoad;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoderoadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
